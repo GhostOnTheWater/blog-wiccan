b79880b8d6e69e1150dc32dc5ccb7b57e003c792
GwordCC <michael.manullang29@gmail.com>
2026-01-29 14:26:28 +0700

    added horror theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tales from the Shadows';
+export const SITE_DESCRIPTION = 'Where darkness meets the page. A chilling collection of stories, thoughts, and mysteries from beyond.';
